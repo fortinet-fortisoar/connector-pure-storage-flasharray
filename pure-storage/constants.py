5734973f58536b506024dc13ca54e1604bd27a31
spathan <spathan@fortinet.com>
2023-08-22 18:11:31 +0530

    added new action and updated authentication

--- pure-storage/constants.py
+++ pure-storage/constants.py
@@ -3,15 +3,21 @@
   All rights reserved.
   FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
   Copyright end """
+
+API_VERSION = 2.23
 AUTHORIZATION_ENDPOINT = '/oauth2/1.0/token'
-ALERTS_ENDPOINT = '/api/1.latest/alerts'
-ARRAYS_ENDPOINT = '/api/1.latest/arrays'
-CONTROLLER_ENDPOINT = '/api/1.latest/controllers'
-DIRECTORY_ENDPOINT = '/api/1.latest/directories'
-DRIVE_ENDPOINT = '/api/1.latest/drives'
+ALERTS_ENDPOINT = '/api/{0}/alerts'
+ARRAYS_ENDPOINT = '/api/{0}/arrays'
+CONTROLLER_ENDPOINT = '/api/{0}/controllers'
+DIRECTORY_ENDPOINT = '/api/{0}/directories'
+DRIVE_ENDPOINT = '/api/{0}/drives'
+AUDIT_ENDPOINT = '/api/{0}/audits'
+VOLUME_ENDPOINT = '/api/{0}/volumes'
+PROTECTION_GROUP_ENDPOINT = '/api/{0}/protection-groups'
+SESSION_ENDPOINT = '/api/{0}/sessions'
 
 GRANT_TYPE = "urn:ietf:params:oauth:grant-type:token-exchange"
 SUBJECT_TOKEN_TYPE = "urn:ietf:params:oauth:token-type:jwt"
-
+MAX_REQUEST_TIMEOUT = 600
 
 
